9817d89e7d051444544f2296686572914de6a845
Georgi Kamacharov <gkamacharov@gmail.com>
2025-06-05 15:13:21 -0400

    added redis cache

--- FinancialHelper/FinancialHelper.AppHost/Program.cs
+++ FinancialHelper/FinancialHelper.AppHost/Program.cs
@@ -2,8 +2,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var databaseBuilder = builder.AddPostgres("Database");
 
+var cacheBuilder = builder.AddRedis("Cache")
+                   .WithRedisInsight();
+
 var apiService = builder.AddProject<Projects.FinancialHelper_ApiService>("Microservice1")
-    .WithReference(databaseBuilder);
+    .WithReference(databaseBuilder)
+    .WithReference(cacheBuilder);
 
 builder.AddProject<Projects.FinancialHelper_Web>("UI")
     .WithExternalHttpEndpoints()
